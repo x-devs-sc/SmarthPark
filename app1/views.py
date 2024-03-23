d977ee0df6d8732a7a5b9d1b90b03ae10c3aebe5
Wuelliton96 <wcsantos100@gmail.com>
2024-03-23 01:21:50 -0300

    feat: Validação de login

--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 
 def IndexPage(request):
     return render(request,'index.html')
